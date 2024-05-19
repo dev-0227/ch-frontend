b0a3c05a14ea7d10f113e1b7d38038355a0e0e4b
Gbapy <mercury.specter@outlook.com>
2024-05-19 12:14:06 -0400

    fixed / app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//app.disable("x-powered-by");
+app.disable("x-powered-by");
 //app.use(cors());
 
 
