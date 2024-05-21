d8741fd05c3566f3795df9310955e213ac768a58
Gbapy <mercury.specter@outlook.com>
2024-05-21 12:40:24 -0400

    fixed / back up cors

--- app.js
+++ app.js
@@ -17,14 +17,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Config middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// disable cors error
-// app.disable("x-powered-by");
-// app.use(cors());
-
-
+app.disable("x-powered-by");
+app.use(cors());
 
 // Static Pages
 app.use('/assets', express.static(path.join(__dirname, 'views/assets')));
