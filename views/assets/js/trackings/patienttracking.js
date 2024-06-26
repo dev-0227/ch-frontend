175e96cb62ae73fa71492fa435fffc2e14aaf194
Gbapy <mercury.specter@outlook.com>
2024-06-26 14:03:51 +0900

    add : monthly in pt tracking

--- views/assets/js/trackings/patienttracking.js
+++ views/assets/js/trackings/patienttracking.js
@@ -1,15 +1,31 @@
 
+var _month = new Date(Date.now()).toISOString().substr(5, 2)
+var _clinicid = localStorage.getItem('chosen_clinic')
+
 $(document).ready(async function() {
     'use strict'
 
-    sendRequestWithToken('POST', localStorage.getItem('authToken'), {id: localStorage.getItem('chosen_clinic')}, "clinic/chosen", (xhr, err) => {
-        if (!err) {
-            let result = JSON.parse(xhr.responseText)['data']
-            if (result.length) {
-                $("#clinic-name").html(result[0].name)
+    if (localStorage.getItem('usertype') > 1) {
+        sendRequestWithToken('POST', localStorage.getItem('authToken'), {id: localStorage.getItem('chosen_clinic')}, "clinic/chosen", (xhr, err) => {
+            if (!err) {
+                let result = JSON.parse(xhr.responseText)['data']
+                if (result.length) {
+                    $("#clinic-name").html(`| ${result[0].name}`)
+                }
             }
+        });
+    }
+
+    renderYearSelect2()
+    function renderYearSelect2() {
+        var year = new Date(Date.now()).getFullYear()
+        var options = ``
+        for (var i = 2000; i <= 2099; i ++) {
+            options += `<option value='${i}'>${i}</option>`
         }
-    });
+        $('#calendar-year').html(options)
+        $('#calendar-year').val(year).trigger('change')
+    }
 
     // Load Appointment Type
     sendRequestWithToken('GET', localStorage.getItem('authToken'), {}, "referral/appointmentType", (xhr, err) => {
@@ -41,11 +57,13 @@ $(document).ready(async function() {
             'type': 'GET',
             'headers': {'Authorization': localStorage.getItem('authToken')},
             'data': (d) => {
-                d.clinicid = localStorage.getItem('chosen_clinic'),
+                d.clinicid = _clinicid,
                 d.visittype = $("#search_type").val(),
                 d.visitstatus = $('#search_status').val(),
                 d.ptseen = $('#search_seen').prop('checked'),
-                d.all = $('#search_all').val()
+                d.all = $('#search_all').val(),
+                d.year = $('#calendar-year').val(),
+                d.month = _month
             }
         },
         serverSide: true,
@@ -81,9 +99,9 @@ $(document).ready(async function() {
             render: (data, type, row) => {
                 return `
                 <div class='d-flex'>
-                    <i class='fa fa-thin text-primary  fa-file-lines p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
-                    <i class='fa fa-thin text-primary fa-file p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
-                    <i class='fa fa-thin text-danger fa-trash-can p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
+                    <i class='fa fa-thin text-primary fa-file-lines p-1 cursor-pointer' style='font-size: 1.3rem;'></i>
+                    <i class='fa fa-thin text-primary fa-file p-1 cursor-pointer' style='font-size: 1.3rem;'></i>
+                    <i class='fa fa-thin text-danger fa-trash-can p-1 cursor-pointer' style='font-size: 1.3rem;'></i>
                 </div>
                 `
             }
@@ -92,10 +110,10 @@ $(document).ready(async function() {
             render: (data, type, row) => {
                 return `
                     <div class='d-flex'>
-                        <i class='fa fa-thin text-primary fa-print p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
-                        <i class='fa fa-thin text-primary fa-envelope p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
-                        <i class='fa fa-thin text-primary fa-mobile-screen p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
-                        <i class='fa fa-thin text-primary fa-phone p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
+                        <i class='fa fa-thin text-primary fa-print p-1 cursor-pointer' style='font-size: 1.2rem;'></i>
+                        <i class='fa fa-thin text-primary fa-envelope p-1 cursor-pointer' style='font-size: 1.2rem;'></i>
+                        <i class='fa fa-thin text-primary fa-mobile-screen p-1 cursor-pointer' style='font-size: 1.2rem;'></i>
+                        <i class='fa fa-thin text-primary fa-phone p-1 cursor-pointer' style='font-size: 1.2rem;'></i>
                     </div>
                 `
             }
@@ -109,8 +127,8 @@ $(document).ready(async function() {
             render: (data, type, row) => {
                 return `
                     <div class='d-flex'>
-                        <i class='fa fa-thin text-primary fa-regular fa-eye p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
-                        <i class='fa fa-solid text-primary fa-list p-1 cursor-pointer' style='font-size: 1.4rem;'></i>
+                        <i class='fa fa-thin text-primary fa-regular fa-eye p-1 cursor-pointer' style='font-size: 1.2rem;'></i>
+                        <i class='fa fa-solid text-primary fa-list p-1 cursor-pointer' style='font-size: 1.2rem;'></i>
                     </div>
                 `
             }
@@ -144,7 +162,7 @@ $(document).ready(async function() {
         }]
     })
 
-    // Search begin //
+    // Patient Search begin //
     $(document).on('keyup', '#search_all', (e) => {
         patientTable.search(e.target.value).draw()
     })
@@ -160,5 +178,289 @@ $(document).ready(async function() {
     $(document).on('change', '#search_status', e => {
         patientTable.search(e.target.value).draw()
     })
-    // Search end //
+    // Patient Search end //
+
+    // Calendar Table begin //
+    var calendarTable = $('#tracking_calendar_table').DataTable({
+        'ajax': {
+            'url': serviceUrl + 'patientlist/statistic',
+            'type': 'GET',
+            'headers': {'Authorization': localStorage.getItem('authToken')},
+            'data': (d) => {
+                d.usertype = localStorage.getItem('usertype')
+                d.clinicid = localStorage.getItem('chosen_clinic')
+                d.year = $('#calendar-year').val()
+            }
+        },
+        serverSide: true,
+        'pageLength': 10,
+        'order': [],
+        'columns': [{
+            data: 'clinicname',
+            render: (data, type, row) => {
+                return `
+                    <a href='#' class='btn btn-link btn-color-gray-700 btn-active-color-primary' id='calendar-clinic-name' data-id='${row.id}'>${row.clinicname}</a>
+                `
+            }
+        }, {
+            data: 'jan',
+            render: (data, type, row) => {
+                if (row.s[0].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[0].month}'><span class='fs-1'>${row.s[0].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'feb',
+            render: (data, type, row) => {
+                if (row.s[1].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[1].month}'><span class='fs-1'>${row.s[1].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'mar',
+            render: (data, type, row) => {
+                if (row.s[2].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[2].month}'><span class='fs-1'>${row.s[2].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'apr',
+            render: (data, type, row) => {
+                if (row.s[3].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[3].month}'><span class='fs-1'>${row.s[3].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'may',
+            render: (data, type, row) => {
+                if (row.s[4].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[4].month}'><span class='fs-1'>${row.s[4].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'jun',
+            render: (data, type, row) => {
+                if (row.s[5].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[5].month}'><span class='fs-1'>${row.s[5].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'jul',
+            render: (data, type, row) => {
+                if (row.s[6].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[6].month}'><span class='fs-1'>${row.s[6].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'aug',
+            render: (data, type, row) => {
+                if (row.s[7].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[7].month}'><span class='fs-1'>${row.s[7].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'sep',
+            render: (data, type, row) => {
+                if (row.s[8].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[8].month}'><span class='fs-1'>${row.s[8].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'oct',
+            render: (data, type, row) => {
+                if (row.s[9].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[9].month}'><span class='fs-1'>${row.s[9].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'nov',
+            render: (data, type, row) => {
+                if (row.s[10].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[10].month}'><span class='fs-1'>${row.s[10].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }, {
+            data: 'dec',
+            render: (data, type, row) => {
+                if (row.s[11].count < 1) {
+                    return `
+                        <div class='text-center'>
+                            <i class="ki-duotone ki-cross text-danger fs-2tx">
+                                <span class='path1'></span>
+                                <span class='path2'></span>
+                            </i>
+                        </div>
+                    `
+                } else {
+                    return `
+                        <div class='text-center'>
+                            <a href='#card-patient' id='calendar-patient-count' class='btn btn-link btn-color-gray-700 btn-active-color-primary' data-kt-scroll-toggle clinic-id='${row.id}' month-id='${row.s[11].month}'><span class='fs-1'>${row.s[11].count}</span></a>
+                        </div>
+                    `
+                }
+            }
+        }]
+    })
+
+    $(document).on('click', '#calendar-clinic-name', function() {
+    })
+
+    $('#calendar-year').on('change', e => {
+        calendarTable.search('').draw()
+
+        patientTable.search($('#search_all').val()).draw()
+    })
+
+    $(document).on('click', '#calendar-patient-count', function() {
+        _month = $(this).attr('month-id')
+        _clinicid = $(this).attr('clinic-id')
+
+        patientTable.search($('#search_all').val()).draw()
+
+        $('#clinic-name').html($(this).parent().parent().parent().children(0).children(0)[0].text)
+    })
+    // Calendar Table end //
 })
