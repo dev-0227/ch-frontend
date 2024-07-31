f259d4cdbf659635961b128542ac5344516a8ef3
Gbapy <mercury.specter@outlook.com>
2024-07-31 11:38:41 +0900

    add : verification for vital loading

--- views/assets/js/loaders/vitalloader.js
+++ views/assets/js/loaders/vitalloader.js
@@ -18,12 +18,18 @@ function uploadCSVFile() {
         let data = JSON.parse(xhr.responseText)
         KTApp.hidePageLoading()
 
+        var type = $('#vital-select option:selected').text()
+        $('#curvital-text').html(`${new Date(Date.now()).getFullYear()} Loaded ${type}`)
+        $('#newvital-text').html(`New Loaded ${type}`)
+        $('#totalvital-text').html(`Total PT Vital`)
+
         const countup_curvital = new countUp.CountUp("kt-countup-curvital")
         const countup_newvital = new countUp.CountUp("kt-countup-newvital")
         const countup_total = new countUp.CountUp("kt-countup-total")
         countup_curvital.update(data.readCount)
         countup_newvital.update(data.addCount)
         countup_total.update(data.total)
+
         $("#load-result-modal").modal("show")
       } else {
         return toastr.error('Action Failed')
@@ -53,6 +59,34 @@ function uploadCSVFile() {
     })
 
     $('#vitalloadbtn').click(function() {
+      $('#verify-vital-name').html('Correct Vital Type is ' + $('#vital-select option:selected').text())
+      sendRequestWithToken('POST', localStorage.getItem('authToken'), {id: localStorage.getItem('chosen_clinic')}, 'clinic/chosen', (xhr, err) => {
+        if (!err) {
+          var result = JSON.parse(xhr.responseText)['data']
+          $('#verify-clinic-name').html('Correct Clinic is ' + result[0].name)
+        }
+        $('#loader-verification').modal('show')
+      })
+    })
+
+    $(document).on('change', '#verify-check-clinic', (e) => {
+      if (e.target.checked == true && $('#verify-check-vital').prop('checked') == true) {
+        $('#verify-done').prop('disabled', false)
+      } else {
+        $('#verify-done').prop('disabled', true)
+      }
+    })
+
+    $(document).on('change', '#verify-check-vital', (e) => {
+      if (e.target.checked == true && $('#verify-check-clinic').prop('checked') == true) {
+        $('#verify-done').prop('disabled', false)
+      } else {
+        $('#verify-done').prop('disabled', true)
+      }
+    })
+
+    $('#verify-done').click(function() {
+      $('#loader-verification').modal('hide')
       uploadCSVFile()
     })
     // Excel End //
