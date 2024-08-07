feb0c44b382987bcde471b537608bbf9469cb8cc
Gbapy <mercury.specter@outlook.com>
2024-08-08 01:57:33 +0900

    add : progress bar for pt loading

--- views/assets/js/loaders/ptloader.js
+++ views/assets/js/loaders/ptloader.js
@@ -1,4 +1,8 @@
 
+let _interval = null
+let _value = 0
+let _isReceivedRequest = true
+
 function uploadCSVFile() {
   var formData = new FormData();
   formData.append("clinicid", localStorage.getItem('chosen_clinic'));
@@ -11,28 +15,67 @@ function uploadCSVFile() {
         formData.append("ptfile", document.getElementById('ptfile').files[i]);
     }
     $(".progress-load").removeClass("d-none");
-    sendFormWithToken('POST', localStorage.getItem('authToken'), formData, "patientlist/ptloader", (xhr, err) => {
-        if (!err) {
-          $(".pt-loader").addClass("d-none");
-          let data = JSON.parse(xhr.responseText)['data'];
-          // $(".totalcount").html(data[0]['total']);
-          // $(".newptcnt").html(data[0]['updated']);
 
-          const countup_curpt = new countUp.CountUp("kt-countup-curpt");
-          const countup_newpt = new countUp.CountUp("kt-countup-newpt");
-          const countup_total = new countUp.CountUp("kt-countup-total");
-          countup_curpt.update(data[0]['total'] - data[0]['updated'])
-          countup_newpt.update(data[0]['updated'])
-          countup_total.update(data[0]['total'])
-          $("#load-result-modal").modal("show");
-        } else {
-          $(".pt-loader").addClass("d-none");
-          return toastr.error('Action Failed');
-        }
+    _interval = setInterval(() => {
+      _value = 0
+      const progressBar = document.querySelector('.kt-progress-bar')
+      const progressText = document.querySelector('.kt-progress-text')
+      progressBar.style.width = 0 + '%'
+      progressBar.setAttribute('aria-valuenow', 0)
+      progressText.textContent = 0 + '%'
+      if (_isReceivedRequest) upgradeProgressbar()
+    }, 750)
+    
+    sendFormWithToken('POST', localStorage.getItem('authToken'), formData, "patientlist/ptloader", (xhr, err) => {
+      if (!err) {
+        // initialize progress bar
+        _value = 0
+        clearInterval(_interval)
+
+        $(".pt-loader").addClass("d-none");
+        let data = JSON.parse(xhr.responseText)['data'];
+        const countup_curpt = new countUp.CountUp("kt-countup-curpt");
+        const countup_newpt = new countUp.CountUp("kt-countup-newpt");
+        const countup_total = new countUp.CountUp("kt-countup-total");
+        countup_curpt.update(data[0]['total'] - data[0]['updated'])
+        countup_newpt.update(data[0]['updated'])
+        countup_total.update(data[0]['total'])
+        $("#load-result-modal").modal("show");
+      } else {
+        $(".pt-loader").addClass("d-none");
+        return toastr.error('Action Failed');
+      }
     });
   } else {
     return toastr.info('Please load file');
   }
+}
+
+const upgradeProgressbar = () => {
+  if (_value >= 100) {
+    clearInterval(_interval)
+    _value = 0
+    return
+  }
+
+  _isReceivedRequest = false
+  sendRequestWithToken('POST', localStorage.getItem('authToken'), {}, 'patientlist/progress', (xhr, err) => {
+    if (!err) {
+      _isReceivedRequest = true
+      _value = JSON.parse(xhr.responseText)['data']
+      const progressBar = document.querySelector('.kt-progress-bar')
+      const progressText = document.querySelector('.kt-progress-text')
+      
+      // Validate percentage
+      if (_value < 0) _value = 0
+      else if (_value >= 100) _value = 100
+
+      // Update progress bar width and text
+      progressBar.style.width = _value.toFixed() + '%'
+      progressBar.setAttribute('aria-valuenow', _value.toFixed())
+      progressText.textContent = _value.toFixed() + '%'
+    }
+  })
 }
 
 $(document).ready(async function () {
